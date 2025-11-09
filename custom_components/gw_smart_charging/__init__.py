6ecbfe265fde4a5e1fdf672714d71d71af8e261a
someone11221 <martin.rak.vy@gmail.com>
2025-11-09 18:08:59 +0100

    Add docstrings to setup functions

--- custom_components/gw_smart_charging/__init__.py
+++ custom_components/gw_smart_charging/__init__.py
@@ -11,13 +11,16 @@ _LOGGER = logging.getLogger(__name__)
 
 
 async def async_setup(hass: HomeAssistant, config) -> bool:
+    """Set up integration (module import)."""
     hass.data.setdefault(DOMAIN, {})
     return True
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Set up for a config entry."""
     hass.data.setdefault(DOMAIN, {})
 
+    # local imports to avoid startup side-effects
     from .coordinator import GWSmartCoordinator
     from .services import async_setup_services
 
@@ -38,6 +41,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Unload a config entry."""
     unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
     if unload_ok:
         hass.data[DOMAIN].pop(entry.entry_id, None)
